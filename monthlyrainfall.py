eb5619609e4eaf1ea2baf99688aaf78fc9459082
Makeda <lilmakfergie@gmail.com>
2020-01-14 17:15:57 +0100

    included total rainfall calculation and created json file

--- monthlyrainfall.py
+++ monthlyrainfall.py
@@ -3,6 +3,11 @@ with open('precipitation.json', encoding = 'utf8') as file:
     precipitation_data = json.load(file)
 
 months= [0,0,0,0,0,0,0,0,0,0,0,0]
+total_precipitation = 0 
+for measurement in precipitation_data:
+    if measurement['station'] == 'GHCND:US1WAKG0038':
+        total_precipitation += measurement['value']
+print(total_precipitation)      
 
 for measurement in precipitation_data:
     if measurement['station'] == 'GHCND:US1WAKG0038':
@@ -12,3 +17,7 @@ for measurement in precipitation_data:
         index_for_our_list = int(dateparts[1]) - 1
         months[index_for_our_list] = months[index_for_our_list] + rainfall_of_this_measurement
         print(months)
+
+
+with open('precipitation_output.json', 'w', encoding = 'utf8') as file:
+    json.dump(total_precipitation, file)
